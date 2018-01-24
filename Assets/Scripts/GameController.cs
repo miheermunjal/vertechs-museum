cff4bd30a282bb883f53eb600aa2290c588ee78d
Miheer Munjal <miheer.munjal@gmail.com>
2018-01-24 15:33:16 -0800

    WIP scripts for game controller and hitboxcollisions

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using HoloToolkit.Unity;
+
+public class GameController : Singleton<GameController>
+{
+    private bool tourOver;
+    private int exhibitCount;
+    public GUIText tourOverText;
+
+    void Start()
+    {
+        tourOver = false;
+        exhibitCount = 0;
+        tourOverText.text = "";
+    }
+
+    public void addExhibit()
+    {
+        if (tourOver != true)
+        {
+            exhibitCount += 1;
+            TourOver();
+        }
+    }
+
+    public void TourOver()
+    {
+        if (exhibitCount > 5)
+        {
+            Camera.main.GetComponent<AudioSource>().Play();
+            tourOverText.text = "Tour Over!";
+            tourOver = true;
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Camera.main.GetComponent<AudioSource>().Pause();
+    }
+
+    public void Help()
+    {
+        //insert commands to play help audio
+    }
+}
---
+++ Assets/Scripts/hitboxcollision.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitboxcollision : MonoBehaviour {
+
+    public const int exhibitCount = 1;
+    private GameController gameController;
+    AudioSource audioSource = null;
+    AudioClip enterZoneSound = null;
+    AudioClip exitZoneSound = null;
+
+    // Use this for initialization
+    void Start () {
+
+         // Add an AudioSource component and set up some defaults
+         audioSource = gameObject.AddComponent<AudioSource>();
+         audioSource.playOnAwake = false;
+         audioSource.spatialize = true;
+         audioSource.spatialBlend = 1.0f;
+         audioSource.dopplerLevel = 0.1f;
+         audioSource.rolloffMode = AudioRolloffMode.Custom;
+
+         // Load the sounds from the Resources folder
+         enterZoneSound = Resources.Load<AudioClip>("female voices");
+         exitZoneSound = Resources.Load<AudioClip>("chimes");
+
+         //find Game Object per each collision
+         GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+         if (gameControllerObject != null)
+         {
+                gameController = gameControllerObject.GetComponent<GameController>();
+         }
+         if (gameController == null)
+         {
+             Debug.Log("Cannot find 'GameController' script");
+         }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "EXHIBIT")
+        {
+            //Destroy(col.gameObject);
+            gameController.addExhibit();
+            audioSource.clip = enterZoneSound;
+            audioSource.volume = 0.3f;
+            audioSource.Play();
+        }
+
+    }
+}
