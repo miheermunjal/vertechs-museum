4efb419614ea1161c0a3fb32f620241448ce8a4c
Yury Kurin <yuriykurin@gmail.com>
2018-01-25 11:45:34 -0800

    Added some scripts

---
+++ Assets/Scripts/SyncMarkerClickEvent.cs
@@ -0,0 +1,23 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncMarkerClickEvent : MonoBehaviour, IInputClickHandler
+{
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        var camera = GameObject.FindWithTag("MainCamera");
+        var marker = GameObject.FindWithTag("SyncMarker");
+        var room = GameObject.FindWithTag("Room");
+
+        var deltaRotationY = marker.transform.rotation.eulerAngles.y - transform.rotation.eulerAngles.y;
+        room.transform.RotateAround(marker.transform.position, Vector3.up, deltaRotationY);
+
+        var deltaPosition = marker.transform.position - transform.position;
+        room.transform.position -= deltaPosition;
+
+
+
+    }
+}
