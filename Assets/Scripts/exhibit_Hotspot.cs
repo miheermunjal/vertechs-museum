50473dcc003dc7c6a3a00442f659d5ba19013283
Miheer Munjal <miheer.munjal@gmail.com>
2018-01-25 10:25:47 -0800

    added collisions, audio plays, room mesh, museum overlay

---
+++ Assets/Scripts/exhibit_Hotspot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exhibit_Hotspot : MonoBehaviour {
+
+    public delegate void HotspotEntered();
+    public static event HotspotEntered OnEntered;
+    public string hotspotTag;
+    private AudioSource audioSource;
+
+    public delegate void HotspotExited();
+    public static event HotspotExited OnExited;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("entering hitzone");
+        var audioObject = GameObject.FindWithTag(hotspotTag);
+        audioSource = audioObject.GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exiting cube");
+        if (OnExited != null)
+        {
+            OnExited();
+        }
+    }
+}
